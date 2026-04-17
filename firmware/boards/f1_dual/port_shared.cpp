59a670757b28d95b19858ec6ffd5c502a59427b5
AlphaX <normsp@gmail.com>
2026-04-16 21:21:01 -0400

    Change CAN configuration from 500 to 1000

--- firmware/boards/f1_dual/port_shared.cpp
+++ firmware/boards/f1_dual/port_shared.cpp
@@ -2,15 +2,15 @@
 
 // board-specific stuff shared between bootloader and firmware
 
-static const CANConfig canConfig500 =
+static const CANConfig canConfig1000 =
 {
     CAN_MCR_ABOM | CAN_MCR_AWUM | CAN_MCR_TXFP,
     /*
      For 24MHz http://www.bittiming.can-wiki.info/ gives us Pre-scaler=3, Seq 1=13 and Seq 2=2. Subtract '1' for register values
     */
-    CAN_BTR_SJW(0) | CAN_BTR_BRP(2)  | CAN_BTR_TS1(12) | CAN_BTR_TS2(1),
+   CAN_BTR_SJW(0) | CAN_BTR_BRP(1) | CAN_BTR_TS1(14) | CAN_BTR_TS2(1),
 };
 
 const CANConfig& GetCanConfig() {
-    return canConfig500;
+    return canConfig1000;
 }
